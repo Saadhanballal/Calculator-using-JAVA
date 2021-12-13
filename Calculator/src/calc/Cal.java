5510d8a83f27eed6482f0e22ece41acac2eda54e
Saadhan Ballal <88136580+Saadhanballal@users.noreply.github.com>
2021-12-13 23:53:48 +0530

    added Calculator

---
+++ Calculator/src/calc/Cal.java
@@ -0,0 +1,43 @@
+package calc;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Cal {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Cal window = new Cal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Cal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ Calculator/src/module-info.java
@@ -0,0 +1,3 @@
+module calculator {
+	requires java.desktop;
+}
